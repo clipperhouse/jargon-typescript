d12275b30edd2f4c5c69be2ffbc3900dadf85cab
Matt Sherman <mwsherman@gmail.com>
2020-02-09 19:55:33 -0500

    ok, just typescript for npm

--- dictionary.d.ts
+++
@@ -1,4 +0,0 @@
-export interface Dictionary {
-    Lookup(input: string[]): string | null;
-    maxGramLength: number;
-}
--- jargon.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference lib="es2015" />
-import { Token } from "./token";
-import { Tokens } from "./tokenizer";
-import { Lemmatizer } from "./lemmatizer";
-import { Dictionary } from "./dictionary";
-export declare function Tokenize(input: string): Tokens;
-export { Token };
-export { Lemmatizer, Dictionary };
